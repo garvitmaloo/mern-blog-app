58ff45c34d7374f54df9ac213a0457183ba11c6e
garvit <garvitmaloo.21@gmail.com>
2023-02-03 13:36:11 +0530

    Added the hero section

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -7,11 +7,19 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@600;700;800;900&family=Poppins:wght@500;600;700;800;900&display=swap"
           rel="stylesheet"
         ></link>
+
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css"
+          integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
 
         <title>The Blogger</title>
 
@@ -19,6 +27,14 @@ export default function App({ Component, pageProps }) {
           defer
           src="https://kit.fontawesome.com/e8be5a49bb.js"
           crossorigin="anonymous"
+        ></script>
+
+        <script
+          defer
+          src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js"
+          integrity="sha512-1/RvZTcCDEUjY/CypiMz+iqqtaoQfAITmNSJY17Myp4Ms5mdxPS5UV7iOfdZoxcGhzFbOm6sntTKJppjvuhg4g=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
         ></script>
       </Head>
       <Component {...pageProps} />
---
+++ frontend/pages/index.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <section className="container hero section" id="hero">
+          <div className="row">
+            <div className="col-lg-6 d-flex flex-column justify-content-center">
+              <h1 className={`${styles.heroHeading} mb-4`}>
+                Best <span>technology guides</span> for you.
+              </h1>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                a dolore fugiat! Reiciendis modi nostrum quas odio veritatis
+                quod cum, itaque illo accusantium ea eos! Dignissimos
+                accusantium quod aspernatur magni.
+              </p>
+              <button className="CTABtn align-self-start">
+                Read latest blogs <i class="fa-solid fa-arrow-right-long"></i>
+              </button>
+            </div>
+            <div className="col-lg-6">
+              <Image
+                className={styles.heroImg}
+                src="/tech.png"
+                alt="A technology illustration"
+                height={600}
+                width={600}
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}

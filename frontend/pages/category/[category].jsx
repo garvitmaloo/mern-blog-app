0cfba3ff9ed1697377748c99c6b682cd842ca36c
garvit <garvitmaloo.21@gmail.com>
2023-02-04 17:59:21 +0530

    Added the search results page

--- frontend/pages/category/[category].jsx
+++ frontend/pages/category/[category].jsx
@@ -1,7 +1,18 @@
 import { useRouter } from "next/router";
 
+import CardsGrid from "../../components/CardsGrid/CardsGrid";
+
 export default function SearchResultPage() {
   const router = useRouter();
 
-  return <h1>Result page for {router.query.category}</h1>;
+  const c = router.query.category;
+  const category = c && c.split("-").join(" ");
+
+  return (
+    <div className={`container py-4 px-0`}>
+      <h1 className="mb-5">Showing blogs in {category} category</h1>
+
+      <CardsGrid />
+    </div>
+  );
 }

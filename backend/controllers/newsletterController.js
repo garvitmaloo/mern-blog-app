1576bb57459683a11d213f1cd1bda88792a2b33e
garvit <garvitmaloo.21@gmail.com>
2023-02-08 09:02:11 +0530

    Added newsletter subscription functionality

--- backend/controllers/newsletterController.js
+++ backend/controllers/newsletterController.js
@@ -1,3 +1,19 @@
-exports.subscribeToNewsletter = function (req, res) {
-  res.send("Route working. Getting data from form");
+const Subscriber = require("../model/Subscriber");
+
+exports.subscribeToNewsletter = async function (req, res) {
+  const { email } = req.body;
+
+  const userExists = await Subscriber.findOne({ email });
+
+  if (userExists) {
+    return res
+      .status(403)
+      .json("This email address has already been subscribed.");
+  }
+
+  const newUser = new Subscriber(req.body);
+
+  newUser.save().catch((err) => console.log(err));
+
+  return res.status(201).send("You have been subscribed.");
 };

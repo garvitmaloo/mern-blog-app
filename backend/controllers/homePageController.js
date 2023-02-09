bcc4dc5fd8ca42d19382fe1957357d69c438eb9f
garvit <garvitmaloo.21@gmail.com>
2023-02-09 12:58:44 +0530

    Implemented visitors home page route functionality

--- backend/controllers/homePageController.js
+++ backend/controllers/homePageController.js
@@ -1,8 +1,21 @@
-exports.homePageController = function (req, res) {
-  res
-    .status(200)
-    .json({
-      latest: "Showing latest posts",
-      popular: "Showing most popular posts",
-    });
+const Post = require("../model/Posts");
+
+exports.homePageController = async function (req, res) {
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth();
+
+  const criteriaDate = new Date(`${currentYear}-${currentMonth + 1}-2`);
+
+  // Fetching all posts posted on or after first date of the current month
+  const latestPosts = await Post.find({
+    createdAt: { $gte: criteriaDate },
+  });
+
+  // Fetching all posts marked as popular
+  const popularPosts = await Post.find({ isPopular: true });
+
+  res.status(200).json({
+    latestPosts,
+    popularPosts,
+  });
 };

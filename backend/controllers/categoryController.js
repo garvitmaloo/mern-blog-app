5d7cc142e46de6958d997db58e59dd2d1db688b1
garvit <garvitmaloo.21@gmail.com>
2023-02-09 13:08:59 +0530

    Implemented fetch posts by category functionality

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,5 +1,12 @@
-exports.getPostsForCategory = function (req, res) {
-  const params = req.params;
+const Post = require("../model/Posts");
 
-  res.send(`Showing blogs for ${params.category}`);
+exports.getPostsForCategory = async function (req, res) {
+  try {
+    const categoryName = req.params.category;
+    const reqPosts = await Post.find({ postCategory: categoryName });
+
+    res.status(200).json(reqPosts);
+  } catch (err) {
+    return res.status(404).json("Could not fetch requested posts");
+  }
 };

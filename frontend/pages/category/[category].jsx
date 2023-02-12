f41aa5afad010d7fbf57810e139d15e7f821cf7b
garvit <garvitmaloo.21@gmail.com>
2023-02-12 23:47:08 +0530

    Showing category wise posts and fixing a small bug

--- frontend/pages/category/[category].jsx
+++ frontend/pages/category/[category].jsx
@@ -1,8 +1,9 @@
+import axios from "axios";
 import { useRouter } from "next/router";
 
 import CardsGrid from "../../components/CardsGrid/CardsGrid";
 
-export default function SearchResultPage() {
+export default function SearchResultPage({ filteredPosts }) {
   const router = useRouter();
 
   const c = router.query.category;
@@ -12,7 +13,18 @@ export default function SearchResultPage() {
     <div className={`container py-4 px-0`}>
       <h1 className="mb-5">Showing blogs in {category} category</h1>
 
-      <CardsGrid />
+      <CardsGrid posts={filteredPosts} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { category } = context.params;
+  const { data } = await axios.get(
+    `http://localhost:4000/api/category/${category}`
+  );
+
+  return {
+    props: { filteredPosts: data },
+  };
 }
